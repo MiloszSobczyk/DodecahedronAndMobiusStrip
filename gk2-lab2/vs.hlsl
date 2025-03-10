bb71c4a44aeb73a333b27b7b7ff4936dbfa69445
MiloszS <m.sobczyk.official@gmail.com>
2025-03-10 18:10:48 +0100

    Changes to vs.hlsl

--- gk2-lab2/vs.hlsl
+++ gk2-lab2/vs.hlsl
@@ -14,32 +14,34 @@ cbuffer cbProj : register(b2) //Vertex Shader constant buffer slot 2 - matches s
 	matrix projMatrix;
 };
 
-//TODO : 0.4. Change vertex shader input structure to match new vertex type
+//DONE : 0.4. Change vertex shader input structure to match new vertex type
 struct VSInput
 {
 	float3 pos : POSITION;
-	float3 col : COLOR; // Remove once no longer used
+    float3 normal : NORMAL;
 };
 
-//TODO : 0.5. Change vertex shader output structure to store position, normal and view vectors in global coordinates
+//DONE : 0.5. Change vertex shader output structure to store position, normal and view vectors in global coordinates
 struct PSInput
 {
 	float4 pos : SV_POSITION;
-	float4 col : COLOR; // Remove once no longer used
+    float4 normal : NORMAL;
+    float4 view : VIEW;
 };
+
 PSInput main(VSInput i)
 {
 	PSInput o;
 	float4 pos = mul(worldMatrix, float4(i.pos, 1.0f));
 
-	//TODO : 0.6. Store global (world) position in the output
-
+	//DONE : 0.6. Store global (world) position in the output
 	pos = mul(viewMatrix, pos);
 	o.pos = mul(projMatrix, pos);
 
-	//TODO : 0.7. Calculate output normal and view vectors in global coordinates frame
+	//DONE : 0.7. Calculate output normal and view vectors in global coordinates frame
 	//Hint: you can calculate camera position from inverted view matrix
+    o.normal = normalize(mul(worldMatrix, float4(i.normal, 0.f)));
+    o.view = mul(invViewMatrix, float4(0.f, 0.f, 0.f, 1.f));
 
-	o.col = float4(i.col, 1.0f); // Remove once no longer used
 	return o;
 }
